27320ce9d1955ecb52e935e271016aa009366ae9
Biman Gunarathne <bimangunarathne@outlook.com>
2024-03-12 19:12:33 +0530

    update app.js

---
+++ src/components/nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='header'>
+        <div className='header_col1'>
+            <div className='header_logo'>
+                <span>FORTUNAGLOBAL</span>
+            </div>
+        </div>
+        <div className='header_col2'>
+            <div className='header_search_bar'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
